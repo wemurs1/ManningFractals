2e567526e809dd344c42d4109a5ec67f7c010896
Stuart Murray <stuart.murray.dev@outlook.com>
2022-10-10 17:43:07 +0100

    checkpoint

--- Milestone1/tree_fractal/Form1.cs
+++ Milestone1/tree_fractal/Form1.cs
@@ -20,6 +20,24 @@ namespace tree_fractal
 
         private void drawButton_Click(object sender, EventArgs e)
         {
+            var angle1 = angle1TextBox.Text;
+            var angle2 = angle2TextBox.Text;
+            var depth = depthTextBox.Text;
+            var length = lengthTextBox.Text;
+            var scale = scaleTextBox.Text;
+            var bitmap = new Bitmap(curvePictureBox.Size.Width, curvePictureBox.Size.Height);
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                graphics.Clear(curvePictureBox.BackColor);
+            }
+            curvePictureBox.Image = bitmap;
+        }
+
+        private double DegreesToRadians(double degrees)
+        {
+            var radians = degrees * Math.PI / 180;
+            return radians;
         }
     }
 }
