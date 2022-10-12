07a6be85e88d417a3a6b1b10b0b2e3e4d120439b
Stuart Murray <stuart.murray.dev@outlook.com>
2022-10-12 14:32:31 +0100

    chaos_game basics

---
+++ Milestone2/chaos_game/chaos_game/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace chaos_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.startButton = new System.Windows.Forms.Button();
+            this.chaosPictureBox = new System.Windows.Forms.PictureBox();
+            this.dotsTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chaosPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(364, 12);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // chaosPictureBox
+            // 
+            this.chaosPictureBox.Location = new System.Drawing.Point(12, 55);
+            this.chaosPictureBox.Name = "chaosPictureBox";
+            this.chaosPictureBox.Size = new System.Drawing.Size(776, 383);
+            this.chaosPictureBox.TabIndex = 1;
+            this.chaosPictureBox.TabStop = false;
+            // 
+            // dotsTimer
+            // 
+            this.dotsTimer.Tick += new System.EventHandler(this.dotsTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chaosPictureBox);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "chaos_game";
+            ((System.ComponentModel.ISupportInitialize)(this.chaosPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button startButton;
+        private PictureBox chaosPictureBox;
+        private System.Windows.Forms.Timer dotsTimer;
+    }
+}
---
+++ Milestone2/chaos_game/chaos_game/Form1.cs
@@ -0,0 +1,80 @@
+namespace chaos_game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool Drawing = false;
+        private Random Rand = new();
+        private int NumDotsDrawn = 0;
+        private Bitmap? ChaosBitmap = null;
+        private PointF[]? Points = null;
+        private PointF CurrentPoint;
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (Drawing) Stop();
+            else Start();
+        }
+
+        private void Start()
+        {
+            Drawing = true;
+            startButton.Text = "Stop";
+            var bitmapWidth = chaosPictureBox.Size.Width;
+            var bitmapHeight = chaosPictureBox.Size.Height;
+            ChaosBitmap = new Bitmap(bitmapWidth, bitmapHeight);
+            var margin = 20;
+            using (Graphics graphics = Graphics.FromImage(ChaosBitmap))
+            {
+                graphics.Clear(Color.Black);
+            }
+            chaosPictureBox.Image = ChaosBitmap;
+            Points = new PointF[3];
+            var lowerLeft = new PointF(margin, bitmapHeight - margin);
+            var lowerRight = new PointF(bitmapWidth - margin, bitmapHeight - margin);
+            var upperMiddle = new PointF(bitmapWidth / 2, margin);
+            Points[0] = lowerLeft;
+            Points[1] = lowerRight;
+            Points[2] = upperMiddle;
+            CurrentPoint = new PointF(bitmapWidth / 2, bitmapHeight / 2);
+            NumDotsDrawn = 0;
+            dotsTimer.Start();
+        }
+
+        private void Stop()
+        {
+            Drawing = false;
+            startButton.Text = "Start";
+            dotsTimer.Stop();
+        }
+
+        private void dotsTimer_Tick(object sender, EventArgs e)
+        {
+            if (!Drawing)
+            {
+                dotsTimer.Stop();
+                Console.WriteLine($"NumDotsDrawn: {NumDotsDrawn}");
+                return;
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                DrawDot();
+            }
+            NumDotsDrawn += 100;
+            chaosPictureBox.Refresh();
+        }
+
+        private void DrawDot()
+        {
+            var index = Rand.Next(0, Points!.Length);
+            var referencePoint = Points[index];
+            CurrentPoint = new PointF(Math.Abs(referencePoint.X - CurrentPoint.X), Math.Abs(referencePoint.Y - CurrentPoint.Y));
+            ChaosBitmap!.SetPixel((int)Math.Round(CurrentPoint.X, 0), (int)Math.Round(CurrentPoint.Y, 0), Color.Red);
+        }
+    }
+}
